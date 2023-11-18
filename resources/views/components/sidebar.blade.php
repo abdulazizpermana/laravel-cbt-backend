1c912d48ac3bc68770c11e36aa4d1bb852cb29cf
abdulazizpermana <abdulazizpermana106@gmail.com>
2023-11-18 11:55:15 +0700

    first commit

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,13 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">CBT TPA</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+
+    </aside>
+</div>
