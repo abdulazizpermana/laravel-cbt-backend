537ac4b7ed9da0974ab0cfa93bcbc537fc86d3cf
abdulazizpermana <abdulazizpermana106@gmail.com>
2023-11-28 21:14:41 +0700

    add bank soal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            UserSeeder::class
-
+            UserSeeder::class,
+            SoalSeeder::class,
         ]);
     }
 }
